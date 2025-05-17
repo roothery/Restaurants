39460a4be824e4ebba7c5d2460bc7b6ce9656c2f
Roothery <roothery@gmail.com>
2025-05-17 16:04:09 -0400

    Adicionado subentities (dishes)

---
+++ Restaurants.Application/Dishes/Commands/DeleteDishes/DeleteDishesForRestaurantCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Restaurants.Application.Dishes.Commands.DeleteDishes
+{
+    public class DeleteDishesForRestaurantCommand(int restaurantId) : IRequest
+    {
+        public int RestaurantId { get; } = restaurantId;
+    }
+}
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -19,6 +19,7 @@ namespace Restaurants.Infrastructure.Extensions
 
             services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
             services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+            services.AddScoped<IDishesRepository, DishesRepository>();
         }
     }
 }
