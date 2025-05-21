84adb8d8b9bad506c33d6898ecfde7e78be3b51e
Roothery <roothery@gmail.com>
2025-05-21 13:38:56 -0400

    Corrigido testes CI github actions

--- tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -9,6 +9,7 @@ using Restaurants.API.Tests;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Seeders;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
@@ -19,6 +20,7 @@ namespace Restaurants.API.Controllers.Tests
     {
         private readonly WebApplicationFactory<Program> _factory;
         private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+        private readonly Mock<IRestaurantSeeder> _restaurantsSeederMock = new();
 
         public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
         {
@@ -30,6 +32,11 @@ namespace Restaurants.API.Controllers.Tests
                     services.Replace(ServiceDescriptor
                         .Scoped(typeof(IRestaurantsRepository),
                         _ => _restaurantsRepositoryMock.Object)
+                    );
+
+                    services.Replace(ServiceDescriptor
+                        .Scoped(typeof(IRestaurantSeeder),
+                        _ => _restaurantsSeederMock.Object)
                     );
                 });
             });
